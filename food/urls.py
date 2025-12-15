59d97b704ffddf1a4a6ea9ef3e948339e0403ae4
Nagesh Iatharaju <nageshiatharaju090@gmail.com>
2025-12-15 21:01:09 +0530

    Initial commit - Django project

---
+++ food/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .import views
+app_name= 'food'
+urlpatterns = [
+    path('',views.index,name='index' ),
+    path("<int:item_id>/",views.details,name="details")
+]
---
+++ food/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Item
+# Create your views here.
+def index(request):
+    item_list=Item.objects.all()
+    context={
+        "item_list":item_list,
+    }
+    return render(request,"food\index.html",context)
+
+def details(request,item_id):
+    item=Item.objects.get(pk=item_id)
+    context={
+        "item":item,
+    }
+    return render(request,"food\details.html",context)
